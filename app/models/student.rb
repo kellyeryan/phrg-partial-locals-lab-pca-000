117b0ad5638170d70c9484f5dfc7da75014a76df
kellyeryan <kellyeryan@gmail.com>
2019-11-14 10:11:47 -0500

    Finish partial locals lab

--- app/models/student.rb
+++ app/models/student.rb
@@ -15,4 +15,14 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(student_name)
+    if student_name == ""
+      Student.all
+    else
+      Student.all.find_all do |student|
+        student.name.include?(student_name.capitalize)
+      end
+    end
+  end
 end
